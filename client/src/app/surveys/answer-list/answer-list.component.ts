b74cad8f5e88652c216b9c34883b7592cced0c5b
Din Lanh <langdinhieu@gmail.com>
2019-04-28 22:11:55 -0400

    minor fixes

--- client/src/app/surveys/answer-list/answer-list.component.ts
+++ client/src/app/surveys/answer-list/answer-list.component.ts
@@ -7,21 +7,21 @@ Heeyeong Kim - 300954759
 Hyojin Kim - 300950009
  */
 
-import { Component, OnInit } from "@angular/core";
-import { SurveyListService } from "src/app/services/survey-list.service";
-import { Survey } from "src/app/models/survey";
-import { FlashMessagesService } from "angular2-flash-messages";
-import { Router, ActivatedRoute } from "@angular/router";
-import { User } from "src/app/models/user";
-import { AuthService } from "src/app/services/auth.service";
-import { AnswerListService } from "src/app/services/answer-list.service";
-import { Answer } from "src/app/models/answer";
-import { AngularCsv } from "angular7-csv/dist/Angular-csv";
+import { Component, OnInit } from '@angular/core';
+import { SurveyListService } from 'src/app/services/survey-list.service';
+import { Survey } from 'src/app/models/survey';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router, ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { AnswerListService } from 'src/app/services/answer-list.service';
+import { Answer } from 'src/app/models/answer';
+import { AngularCsv } from 'angular7-csv/dist/Angular-csv';
 
 @Component({
-  selector: "app-answer-list",
-  templateUrl: "./answer-list.component.html",
-  styleUrls: ["./answer-list.component.css"]
+  selector: 'app-answer-list',
+  templateUrl: './answer-list.component.html',
+  styleUrls: ['./answer-list.component.css']
 })
 export class AnswerListComponent implements OnInit {
   searchAnswer: Answer[];
@@ -46,15 +46,15 @@ export class AnswerListComponent implements OnInit {
 
 
   csvOptions = {
-    fieldSeparator: ",",
-    quoteStrings: '"',
-    decimalseparator: ".",
+    fieldSeparator: ',',
+    quoteStrings: '\'',
+    decimalseparator: '.',
     showLabels: true,
     showTitle: true,
-    title: "Report :",
+    title: 'Report :',
     useBom: true,
     noDownload: false,
-    headers: ["_id", "surveyId", "answer1", "answer2", "answer3", "answer4", "answer5", "createdDate"]
+    headers: ['_id', 'surveyId', 'answer1', 'answer2', 'answer3', 'answer4', 'answer5']
   };
 
   ngOnInit() {
@@ -62,7 +62,7 @@ export class AnswerListComponent implements OnInit {
 
 
     this.surveys = new Array<Survey>();
-    this.current = JSON.parse(localStorage.getItem("user"));
+    this.current = JSON.parse(localStorage.getItem('user'));
 
     this.answers = new Array<Answer>();
     this.answer = new Answer();
@@ -82,18 +82,18 @@ export class AnswerListComponent implements OnInit {
     //get correct answerlist first
     this.answerListService.getAnswerList(answer).subscribe(data => {
       if (data.success) {
+        this.flashMessage.show('Report successfully generated', {cssClass: 'alert-success', timeOut: 3000});
         console.log(data.answerList);
         data.answerList.forEach(ans => {
           this.dtAnswer.push({
-            "_id": ans._id,
-            "surveyId": ans.surveyId,
-            "answer1": ans.answer1,
-            "answer2": ans.answer2,
-            "answer3": ans.answer3,
-            "answer4": ans.answer4,
-            "answer5": ans.answer5,
-            "createdDate": ans.createdDate
-          })
+            _id: ans._id,
+            surveyId: ans.surveyId,
+            answer1: ans.answer1,
+            answer2: ans.answer2,
+            answer3: ans.answer3,
+            answer4: ans.answer4,
+            answer5: ans.answer5
+          });
         });
         // download csv and return to survey list
         this.downloadCSV();
@@ -111,7 +111,7 @@ export class AnswerListComponent implements OnInit {
 
 
   public downloadCSV(){
-  //this.dtHolidays : JSONDATA , HolidayList : CSV file Name, this.csvOptions : file options
-  new AngularCsv(this.dtAnswer, "yourReport", this.csvOptions);
-}
+    // this.dtHolidays : JSONDATA , HolidayList : CSV file Name, this.csvOptions : file options
+    new AngularCsv(this.dtAnswer, 'yourReport', this.csvOptions);
+  }
 }
